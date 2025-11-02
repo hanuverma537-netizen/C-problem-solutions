b89dac77800c806e3d3019e470eadd5f2ca05ba7
Hanu Verma <hanuverma537@gmail.com>
2025-11-02 16:38:57 +0530

    Added first leetcode problem: Palindrome Number

---
+++ Leetcode/9-palindrome-number.c
@@ -0,0 +1,24 @@
+/* 
+Problem: Palindrome Number
+Link: https://leetcode.com/problems/palindrome-number/
+ Description: Given an integer x, return true if x is a palindrome, and false otherwise.
+*/
+#include<stdio.h>
+#include<stdbool.h>
+bool isPalindrome(int x) {
+    long long revNum = 0;
+    for (int temp = x; temp > 0; temp /= 10) {
+        int digit = temp % 10;
+        revNum = revNum * 10 + digit;
+    }
+    return revNum == x;
+}
+int main() {
+    int x;
+    scanf("%d", &x);
+    if (isPalindrome(x)) {
+        printf("true\n");
+    } else {
+        printf("false\n");
+    }
+}
