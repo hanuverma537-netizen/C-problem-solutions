03bd1829e5eefc19cd514bf12c95a5b9539d94e3
Hanu Verma <hanuverma537@gmail.com>
2026-03-10 23:11:02 +0530

    Added 87th leetcode problem: String to Integer (atoi)

---
+++ Leetcode/8-string-to-integer-atoi.c
@@ -0,0 +1,41 @@
+/*
+Problem: String to Integer (atoi)
+Link: https://leetcode.com/problems/string-to-integer-atoi/description/
+Description: 
+Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer.
+
+The algorithm for myAtoi(string s) is as follows:
+
+Whitespace: Ignore any leading whitespace (" ").
+Signedness: Determine the sign by checking if the next character is '-' or '+', assuming positivity if neither present.
+Conversion: Read the integer by skipping leading zeros until a non-digit character is encountered or the end of the string is reached. If no digits were read, then the result is 0.
+Rounding: If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then round the integer to remain in the range. Specifically, integers less than -231 should be rounded to -231, and integers greater than 231 - 1 should be rounded to 231 - 1.
+Return the integer as the final result.
+*/
+
+#include<stdio.h>
+#include<limits.h>
+int myAtoi(char* s) {
+    int num = 0, sign = 1, i = 0;
+    while (s[i] == ' ') i++;
+    if (s[i] == '-' || s[i] == '+') {
+        if (s[i] == '-') {
+            sign = -1;
+        }
+        i++;
+    }
+    while (s[i] >= '0' && s[i] <= '9') {
+        if (num > (INT_MAX - (s[i] - '0')) / 10) return sign == 1 ? INT_MAX : INT_MIN;
+        num = num * 10 + (s[i] - '0');
+        i++;
+    }
+    if (num * sign > INT_MAX) return INT_MAX;
+    else if (num * sign < INT_MIN) return INT_MIN;
+    else return num * sign;
+}
+int main() {
+    char s[200];
+    fgets(s, 200, stdin);
+    printf("%d", myAtoi(s));
+    return 0;
+}
